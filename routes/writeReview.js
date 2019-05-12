6a4134de27fb4fec9efa114bb51eca796fee3528
Keerthana Rajendran <kitanaa97@gmail.com>
2019-05-12 19:31:28 -0400

    [Keerthana] fixed client side error

--- routes/writeReview.js
+++ routes/writeReview.js
@@ -1,104 +1,80 @@
 const router = require("express").Router();
 
 const { user, review } = require("../data");
+const auth = require("../middleware/auth");
+var xss = require("xss");
 
 router.get("/", async (req, res) => {
-    //console.log(req.session.username);
-    try{
-        // if(req.session.username){
-        //     //console.log("hitsssss here")
-        //     res.redirect("/private")
-        // }
-        //else{
-            ////console.log("hitsssss herehbdcfahbd  me")
-            const reviewList = await review.getAllReview();
-            //res.render("review",{title:"Reviews" , ReviewList:reviewList, noError:true})
+  //console.log(req.session.username);
+  try {
+    const reviewList = await review.getAllReview();
+    //res.render("review",{title:"Reviews" , ReviewList:reviewList, noError:true})
 
-            if (req.cookies.name === 'AuthCookie') {
-              // const thisUser = await users.getUserById(req.session.user._id)
-              res.render("writeReview",{title:"Reviews" , ReviewList:reviewList, noError:true, auth: true})
-            } 
-            else {
-              res.render("writeReview",{title:"Reviews" , ReviewList:reviewList, noError:true})
-            }
-        //}
-        
+    if (req.cookies.name === "AuthCookie") {
+      // const thisUser = await users.getUserById(req.session.user._id)
+      res.render("writeReview", {
+        cssName: "main",
+        title: "Reviews",
+        ReviewList: reviewList,
+        noError: true,
+        auth: true
+      });
+    } else {
+      res.render("writeReview", {
+        cssName: "main",
+        title: "Reviews",
+        ReviewList: reviewList,
+        noError: true
+      });
     }
-    catch(e){
-        console.log('Error is found ', e);
-  res.sendStatus(500).json({error: e.toString() || 'Server Error', route: req.originalUrl});
+    //}
+  } catch (e) {
+    console.log("Error is found ", e);
+    res
+      .sendStatus(500)
+      .json({ error: e.toString() || "Server Error", route: req.originalUrl });
+  }
+});
+
+router.post("/", async (req, res) => {
+  console.log("aswedfrthy");
+
+  try {
+    let email = xss(req.body.user_emailId);
+    let emailresult = await user.checkUsername(email);
+    if (emailresult) {
+      const newReview = await review.postReview(
+        req.body.user_emailId,
+        req.body.name,
+        req.body.stars,
+        req.body.comment
+      );
+      res.redirect("/writeReview");
+    } else {
+      const reviewList = await review.getAllReview();
+      res.status(401);
+      res.render("writeReview", {
+        cssName: "main",
+        title: " Reviews",
+        errorfound: "Please create an account to add a review",
+        ReviewList: reviewList,
+        noError: true
+      });
     }
 
-  });
-
-  router.post("/", async (req, res) => {
-    console.log("aswedfrthy")
-   
-    try {
-        const newReview = await review.postReview(
-          req.body.user_emailId,
-          req.body.name,
-          req.body.stars,
-          req.body.comment
-        );
-        res.redirect("/writeReview")
-        //res.status(200).json(newReview);
-      } catch (e) {
-        console.log("am just caught here")
-        const reviewList = await review.getAllReview();
-        res.status(401);
-        res.render("writeReview",{title:" Reviews",errorfound:"Please fill all the fields",ReviewList:reviewList, noError:true});
-      }
-    
-    
-    // if(req.session.username){
-    //     //console.log("hitsssss here")
-    //     res.redirect("/private")
-    // }
-    // else {
-    //    if(req.body.username === undefined || req.body.username.length === 0 || req.body.password.length === 0)
-    //    {
-    //     res.status(401);
-    //     res.render("login/login",{title:"Error Found",errorfound:"Error Found enter valid username or password"});
-    //    }
-          //else{
-    //   const userName = req.body.username;
-    //   const passWord = req.body.password;
-    //   let flag = true;
-    //   for(let i = 0; i<userData.length; i++){   
-       
-    //     if(userName === userData[i]['username']){
-    //       //console.log(userName + " " + userData[i]['username'] + "asfafdadfadfadfadfad")
-    //       let pass = await bcrypt.compare(passWord,userData[i]['hashedPassword'])
-    //       //console.log(pass + " " + userData[i]["hashedPassword"]+ " " + passWord)
-    //       if(  pass === true  ){
-            
-    //       //console.log("hits here pocolo")
-    //       flag = false;
-    //       req.session.username = userData[i]['username']
-    //       req.session.firstname = userData[i]['firstName']
-    //       req.session.lastname = userData[i]['lastName']
-    //       req.session._id = userData[i]['_id']
-    //       req.session.profession = userData[i]['profession']
-    //       req.session.bio = userData[i]['bio']
-        
-    //       }
-    //     }
-         
-    //     }                      
-        
-    //     if(flag === true){
-    //         res.status(401).render("login/login",{title: "Login Page", errorfound:"user or password doesnt match  please check"})
-    //     }
-    //     else {
-    //         if (req.session.username){
-    //             res.redirect("/private")}
-    //             }
-       // }
-    // }
-   
-  });
-
-
+    //res.status(200).json(newReview);
+  } catch (e) {
+    console.log("am just caught here");
+    const reviewList = await review.getAllReview();
+    res.status(401);
+    res.render("writeReview", {
+      cssName: "main",
+      title: " Reviews",
+      errorfound: "Please fill all the fields",
+      ReviewList: reviewList,
+      noError: true
+    });
+  }
+});
 
 module.exports = router;
--- seed.js
+++ seed.js
@@ -27,20 +27,7 @@ async function main() {
         const id_2 = keerthana._id;
         const patrick = await user.addUser("patrick@gmail.com","Patrick","$2a$16$4o0WWtrq.ZefEmEbijNCGukCezqWTqz1VWlPm/xnaLM8d3WlS5pnK");
         const id_3 = patrick._id;
-        // const user_email3 = patrick.email;
-        // console.log(user_email3);
-        // const rob = await user.addUser("rob@gmail.com","Phill","$2a$16$7JKSiEmoP3GNDSalogqgPu0sUbwder7CAN/5wnvCWe6xCKAKwlTD.");
-        // const id = rob._id;
-        // // const alex = await user.addUser("phil@gmail.com","Phill","$2a$16$7JKSiEmoP3GNDSalogqgPu0sUbwder7CAN/5wnvCWe6xCKAKwlTD.");
-        // const id = alex._id;
-        // const john = await user.addUser("phil@gmail.com","Phill","$2a$16$7JKSiEmoP3GNDSalogqgPu0sUbwder7CAN/5wnvCWe6xCKAKwlTD.");
-        // const id = john._id;
-        // const rita = await user.addUser("phil@gmail.com","Phill","$2a$16$7JKSiEmoP3GNDSalogqgPu0sUbwder7CAN/5wnvCWe6xCKAKwlTD.");
-        // const id = rita._id;
-        //console.log(phil)
-        // const firstReview = await review.addReview( String(id_1),"2018-05-27","5","The food was very tasty");
-        // const secondReview = await review.addReview( String(id_2),"2018-04-29","4","The food was very yummy");
-        // const thirdReview = await review.addReview( String(id_3),"2018-02-26","3","The food was very spicy");
+       ///////////////PostReview////////////////
         const fourthReview = await review.postReview("phil@gmail.com","Phill Baresi","3","The food was very spicy" )
         const fifthReview = await review.postReview("keerthana@gmail.com", "Keerthana","4","The food was very yummy" )
         const sixthReview = await review.postReview("rob@gmail.com"," Rob","5","The food was very tasty" )
