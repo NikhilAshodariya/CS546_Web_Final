25fc52d8642f4462533799626638de61a1ed6d07
Keerthana Rajendran <kitanaa97@gmail.com>
2019-05-12 04:31:41 -0400

    [Keerthana] fixed bugs

--- public/js/main.js
+++ public/js/main.js
@@ -6,27 +6,33 @@ try {
         const emailInput = document.getElementById("user_emailId");
         const commentInput = document.getElementById("comment");
         const starsInput = document.getElementById("stars");
-
-        console.log("hit6");
-
-    
-        //Add an event listener for the form submit
-        ReviewForm.addEventListener("submit", event => {
-          console.log("hit7");
-          //We need to prevent the default behavior of the form submit
-          event.preventDefault();
-    
-          /*  Now we check to make sure the user entered a value in the input box
-                We are only concerned if they entered something, we do not need to
-                validate if it's a number, string etc.. in this example
-            */
-          if (!emailInput.value || !commentInput.value || !starsInput.value ) {
+        if (!emailInput.value || !commentInput.value || !starsInput.value ) {
             $("#error").show();
             $("#error").html("You Need to supply an input value!");
     
             //then set the cursor focus to the input field
             $("#user_emailId").focus();}
-        });
+
+        console.log("hit6");
+
+    
+        //Add an event listener for the form submit
+        // ReviewForm.addEventListener("submit", event => {
+        //   console.log("hit7");
+        //   //We need to prevent the default behavior of the form submit
+        //   event.preventDefault();
+    
+        //   /*  Now we check to make sure the user entered a value in the input box
+        //         We are only concerned if they entered something, we do not need to
+        //         validate if it's a number, string etc.. in this example
+        //     */
+        //   if (!emailInput.value || !commentInput.value || !starsInput.value ) {
+        //     $("#error").show();
+        //     $("#error").html("You Need to supply an input value!");
+    
+        //     //then set the cursor focus to the input field
+        //     $("#user_emailId").focus();}
+        // });
       }
   $(document).ready(function() {
     $(".input").focus(function() {
